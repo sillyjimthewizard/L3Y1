fb97d5735602e9fb79aa2e2f16197b814adf52f4
sillyjimthewizard <cha24001426@heartofyorkshire.ac.uk>
2025-01-14 16:01:28 +0000

    yummy yumy yummy salt and enemies

--- CarController-main/Assets/Scripts/spawner.cs
+++ CarController-main/Assets/Scripts/spawner.cs
@@ -4,14 +4,35 @@ using UnityEngine;
 
 public class spawner : MonoBehaviour
 {
+    private bool cansalt = true;
+    public int salty =5;
+
     // Start is called before the first frame update
 
     public GameObject cubePrefab;
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Q) && salty > 0)
         {
-            Instantiate(cubePrefab, transform.position, Quaternion.identify);
+
+            salty = (salty - 1);
+            Instantiate(cubePrefab, transform.position, Quaternion.identity);
+
         }
+    
+        if (salty > 5)
+        {
+            salty = (5);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("refill"))
+        {
+            salty = (salty + 5);
+
+        }
+
     }
 }
---
+++ CarController-main/Assets/enemymoveplease.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemymoveplease : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+     [SerializeField] private Transform movePositionTransform;
+
+     private NavMeshAgent navMeshAgent;
+
+
+    private void Awake ()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        navMeshAgent.destination = movePositionTransform.position;
+    }
+}
